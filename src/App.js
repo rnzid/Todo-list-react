51eb00b31383adfae884e20b9e111b4932a60600
rnzid <ranjitdangol13@gmail.com>
2022-05-12 13:05:42 +0545

    Todo list

--- src/App.js
+++ src/App.js
@@ -3,43 +3,48 @@ import Header from "./MyComponents/Header";
 import {Todos} from "./MyComponents/Todos";
 import {Footer} from "./MyComponents/Footer";
 import { AddTodo } from "./MyComponents/AddTodo";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
+    let initTodo;
+    if(localStorage.getItem("todos")===null){
+        initTodo = [];
+    }
+    else{
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+    }
+
     const onDelete = (todo)=>{
         console.log("I am onDelete",todo);
         setTodos(todos.filter((e)=>{
             return e!==todo;
         }))
+        localStorage.getItem("todos");
     }
     const addTodo = (title,desc)=>{
-        console.log("I am adding todo",title,desc)
-        let sno =todos[todos.length-1].sno+1;
+        console.log("I am adding todo",title,desc);
+        let sno;
+        if(todos.length==0){
+            sno = 0;
+        }
+        else{
+            sno =todos[todos.length-1].sno + 1;
+        }
         const myTodo = {
-            sno:sno,
+            sno : sno,
             title : title,
-            desc:desc
+            desc : desc,
         }
         setTodos([...todos,myTodo]);
         console.log(myTodo);
+
     }
-    const [todos, setTodos] = useState( [
-        {
-            sno:1,
-            title:"Go to the market",
-            desc:"You need to go to market to get this job"
-        },
-        {
-            sno:2,
-            title:"Go to the mall",
-            desc:"You need to go to market to get this job2"
-        },
-        {
-            sno:3,
-            title:"Go to the hosp",
-            desc:"You need to go to market to get this job3"
-        },
-    ])
+    const [todos, setTodos] = useState( [initTodo]);
+    useEffect(() => {
+        localStorage.setItem("todos",JSON.stringify(todos));
+    
+    }, [todos])
+    
     return (
         <>
             <Header title="My Todo List" />
